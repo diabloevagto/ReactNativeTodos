7e208cc11c722b6f019240776d591ab38f8ec314
diabloevagto <diabloevagto@gmail.com>
2018-05-08 23:50:55 +0800

    backup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,13 +11,19 @@ import FilterLink from '../containers/FilterLink'
 const Footer = () => (
   <View style={styles.inline}>
     <Text>Show: </Text>
-    <FilterLink filter={VisibilityFilters.SHOW_ALL}>
+    <FilterLink
+      filter={VisibilityFilters.SHOW_ALL}
+    >
       All
     </FilterLink>
-    <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>
+    <FilterLink
+      filter={VisibilityFilters.SHOW_ACTIVE}
+    >
       Active
     </FilterLink>
-    <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>
+    <FilterLink
+      filter={VisibilityFilters.SHOW_COMPLETED}
+    >
       Completed
     </FilterLink>
   </View>
